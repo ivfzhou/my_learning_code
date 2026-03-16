58cc9c4c2e23a99f9b9fc4b1e3a6daf9ea8a9560
ivfzhou <ivfzhou@126.com>
2026-03-16 21:42:47 +0800

    forward reference codes

--- cpp/forward_reference.cpp
+++ cpp/forward_reference.cpp
@@ -1,16 +1,48 @@
+#include <chrono>
+#include <exception>
 #include <format>
 #include <iostream>
+#include <ostream>
+#include <sstream>
+#include <utility>
 
 #include "forward_reference.hpp"
 
 namespace gitee::com::ivfzhou::cpp {
     /*
-    void log() { std::cout << std::endl; }
+    void print() { std::cout << std::endl; }
+
+    std::string now() {
+        return std::format("{:%Y-%m-%d %H:%M:%S}",
+                           std::chrono::current_zone()->to_local(
+                               std::chrono::floor<std::chrono::milliseconds>(std::chrono::system_clock::now())));
+    }
+
+    const auto& one_line_string = []<typename T>(T&& arg) {
+        std::ostringstream temp;
+        temp << std::forward<T>(arg);
+        std::string escaped;
+        for (const char c : temp.str()) {
+            if (c == '\n')
+                escaped += "\\n";
+            else if (c == '\r')
+                escaped += "\\r";
+            else
+                escaped += c;
+        }
+        return escaped;
+    };
 
     template<typename T, typename... U>
-    void log(T&& t, U&&... args) {
+    void print(T&& t, U&&... args) {
         std::cout << t << " ";
-        log(std::forward<U>(args)...);
+        print(std::forward<U>(args)...);
+    }
+
+    template<typename... U>
+    void log(U&&... args) {
+        std::cout << now() << " ";
+        print(std::forward<U>(args)...);
     }
 
     // 显式实例化定义。
@@ -22,9 +54,9 @@ namespace gitee::com::ivfzhou::cpp {
         std::cout << "[pugixml] 开始 - 测试转发引用与模板递归效果" << std::endl;
         try {
             log("INFO", "hello logger", 1, 1.1, false);
-            LOG("ERROR", "macro logger", 1, 1.1, false);
+            LOG("INFO", "macro logger", 1, 1.1, false);
         } catch (std::exception& e) {
-            std::cout << "发生错误：" << e.what() << std::endl;
+            LOG("发生错误：", e.what());
         }
         std::cout << "[pugixml] 结束 - 测试转发引用与模板递归效果" << std::endl << std::endl;
     }
--- cpp/forward_reference.hpp
+++ cpp/forward_reference.hpp
@@ -1,28 +1,65 @@
 #ifndef CPP_FORWARD_REFERENCE_HPP
 #define CPP_FORWARD_REFERENCE_HPP
 
+#include <chrono>
+#include <format>
+#include <iostream>
+#include <ostream>
+#include <sstream>
+#include <string>
+#include <utility>
+
 namespace gitee::com::ivfzhou::cpp {
 
     /*
-    void log();
-    template<typename T, typename... U>
-    void log(T&& t, U&&... args);
+    template<typename... T>
+    void log(T&&... args);
     // 显式实例化声明。
     extern template void log<char const (&)[10]>(char const (&)[10]);
     */
 
-
-    static void log() { std::cout << std::endl; }
+    const static auto& one_line_string = []<typename T>(T&& arg) {
+        std::ostringstream temp;
+        temp << std::forward<T>(arg);
+        std::string escaped;
+        for (const char c : temp.str()) {
+            if (c == '\n')
+                escaped += "\\n";
+            else if (c == '\r')
+                escaped += "\\r";
+            else
+                escaped += c;
+        }
+        return escaped;
+    };
+    static std::string now() {
+        return std::format("{:%Y-%m-%d %H:%M:%S}",
+                           std::chrono::current_zone()->to_local(
+                               std::chrono::floor<std::chrono::milliseconds>(std::chrono::system_clock::now())));
+    }
+    static void print() { std::cout << std::endl; }
     template<typename T, typename... U>
-    void log(T&& t, U&&... args) {
-        std::cout << t << " ";
-        log(std::forward<U>(args)...);
+    void print(T&& t, U&&... args) {
+        std::cout << one_line_string(std::forward<T>(t)) << " ";
+        print(std::forward<U>(args)...);
+    }
+    template<typename... T>
+    void log(T&&... args) {
+        std::cout << now() << " ";
+        print(std::forward<T>(args)...);
     }
 
     // 测试转发引用与模板递归效果。
     void forward_reference_and_recursive_template();
 
-#define LOG(level, ...) log(level, __FILE__, ":", __LINE__, __VA_ARGS__)
+#define LOG(...)                                                                                                       \
+    {                                                                                                                  \
+        auto fileName = std::string(__FILE__);                                                                         \
+        auto index = fileName.find_first_of("cpp");                                                                    \
+        if (index != std::string::npos) fileName = fileName.substr(index + 2);                                         \
+        fileName.append(":").append(std::format("{}", __LINE__));                                                      \
+        log(fileName, __VA_ARGS__);                                                                                    \
+    }
 
 }
 
