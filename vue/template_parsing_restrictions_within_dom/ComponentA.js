1dc1debff352363d9c1dd12e16e1fe07bdb4fe44
ivfzhou <ivfzhou@126.com>
2026-04-22 22:00:19 +0800

    review code

--- vue/template_parsing_restrictions_within_dom/ComponentA.js
+++
@@ -1,8 +0,0 @@
-export default {
-    setup(){
-
-    },
-    template: `
-<span>ComponentA</span>
-`
-}
--- vue/template_parsing_restrictions_within_dom/ComponentB.js
+++
@@ -1,8 +0,0 @@
-export default {
-    setup(){
-
-    },
-    template: `
-<span>ComponentB</span>
-`
-}
